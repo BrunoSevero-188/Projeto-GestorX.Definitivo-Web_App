d67a0e6fcf488502abfcfc038d7a7d4c8a43afef
BrunoSevero-188 <brunosevero188@gmail.com>
2026-06-14 20:06:07 -0400

    Atualização

--- app/telas/TelasInternas/slideBar/Estante/OutrasAcoes/page.tsx
+++ app/telas/TelasInternas/slideBar/Estante/OutrasAcoes/page.tsx
@@ -1,3 +1,6 @@
-﻿export default function OutrasAcoesPage() {
-  return <div>Outras Acoes</div>
+﻿import React from "react";
+
+export default function Page() {
+  return <main style={{ padding: 16 }}>Outras Ações</main>;
 }
+
